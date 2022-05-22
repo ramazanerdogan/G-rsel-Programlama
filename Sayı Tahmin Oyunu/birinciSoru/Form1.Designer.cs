148c2a104830ac3217ae405b234ef4389212b29f
Ramazan Erdoğan <52610482+ramazanerdogan@users.noreply.github.com>
2022-05-23 01:37:03 +0300

    Dört Basamaklı sayı tahmin oyunu
    Küçük çocukların matematiksel zekalarını geliştirmek için verilen dört basamaklı bir sayının Bulunması beklenen bir oyundur. Bu oyun ile kullanıcı her defasında dört basamaklı sayıyı tahmin etmeye çalışacak ve program yaklaşması halinde “+” koordinat uzaklaşması halinde ise “-” koordinat vererek kullanıcıyı yönlendirecektir

---
+++ Sayı Tahmin Oyunu/birinciSoru/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace birinciSoru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {               components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(293, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tahmininizi Giriniz:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 99);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "TAHMİN ET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(293, 248);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "İpucu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Oyun Bitti:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(293, 312);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(198, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(123, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tahmin Sayısı:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(204, 425);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "***Tahmin Hakkı***";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(293, 373);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Tahmin Oyununa Hoşgeldiniz.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(423, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Hadi Bilgisayarın Oluşturduğu Sayıları İpuçları İle Tahmin Edelim:D";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(88, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(376, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "\"+\"İpuçları yer ve sayı doğruluğu,\"-\"ipuçları sayı doğruluğu.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 480);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Sayı Tahmin Oyunu/birinciSoru/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace birinciSoru
+{
+    public partial class Form1 : Form
+    {
+        int[] bilgisayarSectigiSayi;//bir dizi değişkeni tanımladık
+        public void RastgeleSayiTekirdag()// rastgele sayi için bir fonksiyon yazıyoruz.
+        {
+            bilgisayarSectigiSayi = new int[4]; //4 basamaklı olacak
+
+            Random rastgele = new Random();
+            int rastgeleSayi = rastgele.Next(1023, 9876); //random sayi üretiyoruz
+            int sayiBir, sayiIki, sayiUc, sayiDort;
+            sayiBir = rastgeleSayi % 10;
+            sayiIki = (rastgeleSayi / 10) % 10;
+            sayiUc = (rastgeleSayi / 100) % 10;
+            sayiDort = (rastgeleSayi / 1000) % 10;
+            if (sayiBir != sayiIki && sayiBir != sayiUc && sayiBir != sayiDort && sayiIki != sayiUc && sayiIki != sayiDort && sayiUc != sayiDort)
+            {
+                
+                for (int i = 0; i < 4; i++)
+                {
+                    
+                    bilgisayarSectigiSayi[i] = rastgeleSayi % 10;
+                    rastgeleSayi = rastgeleSayi / 10;
+                }
+
+            }//buraya kadarki işlemlerde aldığımız sayıyı basamaklara ayırarak tüm sayıların birbirinden farklı gelmesini sağladık
+            else
+            {
+                RastgeleSayiTekirdag();
+            }
+            
+
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac;// ekrana kaç tahminde bulunduğunu yazmamız için bir sayac tanımladık.
+        private void Form1_Load(object sender, EventArgs e)//form açıldığında yapılacak işlemler
+        {
+            RastgeleSayiTekirdag();//form açıldığında sayı üretilsin istiyoruz.
+            textBox3.Text = sayac.ToString();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)//butona bastıkça olacccak işlemler
+        {
+            sayac++;//butona bastıkça sayacı arttırıcaz.
+            textBox3.Text = sayac.ToString();//sayacı yazdırıcaz.
+
+
+            int i;
+
+            int artiSayiBilgisayarIsmail = 0;
+            int eksiSayiBilgisayarEnis = 0;
+            int[] tahminHafızaSuleymanpasa = new int[4];//değişkenler tanımladık
+
+            int x = 0;
+
+            while (x < 4)//döngü oluşturduk.Her basamak tahmini için 4.
+            {
+                x++;
+                artiSayiBilgisayarIsmail = 0;
+                eksiSayiBilgisayarEnis = 0;//ipucu değişkenleri
+                
+
+
+                int tahminiSayiTekirdagKirazi = Convert.ToInt32(textBox1.Text);//tahmindeki sayılar farklı girilmesi için 
+                int kontrolSayisi = tahminiSayiTekirdagKirazi;                 //rastgelle sayıda yaptığımız işlemleri yaparak
+                int sayiBir, sayiIki, sayiUc, sayiDort;                        //if else ile kontrol ettirdik.
+                sayiBir = kontrolSayisi % 10;
+                sayiIki = (kontrolSayisi / 10) % 10;
+                sayiUc = (kontrolSayisi / 100) % 10;
+                sayiDort = (kontrolSayisi / 1000) % 10;
+                if (sayiBir != sayiIki && sayiBir != sayiUc && sayiBir != sayiDort && sayiIki != sayiUc && sayiIki != sayiDort && sayiUc != sayiDort)
+                {
+
+
+
+
+
+                    for (i = 0; i < 4; i++)//basamaklara göre karşılaştırma oyunu yaparak gerekli değişkenleri arttırdık ve azalttık.
+                    {
+                        tahminHafızaSuleymanpasa[i] = tahminiSayiTekirdagKirazi % 10;
+
+                        tahminiSayiTekirdagKirazi /= 10;
+
+                        bool mevcutRakam = false;//doğru ve yanlış olabilecek bir değişken atadik ve başlangıç yanlış dedik.
+
+                        for (int j = 0; j < 4; j++)
+                        {
+                            if (tahminHafızaSuleymanpasa[i] == bilgisayarSectigiSayi[j])
+                            {
+                                mevcutRakam = true;//karşılaştırma sonucu doğru olduğunda döngüden çık
+                                break;
+                            }
+                        }
+
+                        if (mevcutRakam == true)// true olunca gerekli şartlarda ipucu değişkenlerine işlem yaptır.
+                        {
+                            if (tahminHafızaSuleymanpasa[i] == bilgisayarSectigiSayi[i])
+                            {
+                                artiSayiBilgisayarIsmail++;
+                            }
+                            else
+                            {
+                                eksiSayiBilgisayarEnis++;
+                            }
+                        }
+
+                    }
+
+                    textBox2.Text = Convert.ToString("+" + artiSayiBilgisayarIsmail + "," + "-" + eksiSayiBilgisayarEnis);//ipuçlarını yazdır.
+
+                    if (artiSayiBilgisayarIsmail == 4)//yani tüm değerler doğru olduğunda kontrol et
+                    {
+                        textBox4.Text = Convert.ToString("TEBRİKLER...Oyun Bitti...");//oyun bitti bilgisi ver.
+                        label6.Text = (sayac.ToString()) + ".Hakta Oyunu Kazandiniz.";// tahmin hakkını ekranda göster.
+                        
+                    }
+                }
+                else//tahmindeki sayılar aynı girildiğinde verilecek uyarı mesajı.
+                {
+                    MessageBox.Show("Lutfen Tahmindeki Tum Sayilari Farkli Giriniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+
+            }
+
+        }
+
+        
+    }
+}
